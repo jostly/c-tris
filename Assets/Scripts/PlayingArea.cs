bb4dfb8a9785bbb50668fe54035ca40f63594852
Nathalie Cirverius <nathalie.cirverius@postnord.com>
2022-02-17 10:01:42 +0100

    Align stuff

--- Assets/Scripts/PlayingArea.cs
+++ Assets/Scripts/PlayingArea.cs
@@ -31,25 +31,56 @@ public class PlayingArea : MonoBehaviour
             o = Instantiate(borderPrefab, transform);
             o.transform.localPosition = new Vector3(width, y, 0);
         }
-
-        transform.position = new Vector3(-width / 2f, 0, 1);
     }
 
     public bool CanAddShape(int x, int y, Shape shape)
     {
-        return true; // TODO: Kolla om det finns något ivägen för blocket och returnera false i så fall
+        for (var ty = 0; ty < 4; ty++)
+        {
+            for (var tx = 0; tx < 4; tx++)
+            {
+                if (shape.IsBlocked(tx, ty))
+                {
+                    if (!CanAddBlock(x + tx, y + ty))
+                    {
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+
+    public bool CanAddBlock(int x, int y)
+    {
+        if (y < 0 || y >= height)
+        {
+            return false;
+        }
+
+        if (x < 0 || x >= width)
+        {
+            return false;
+        }
+
+        if (_grid[x, y])
+        {
+            return false;
+        }
+
+        return true; // TODO: Kolla om det finns något i vägen för blocket och returnera false i så fall
         // Kolla även om någon del av blocket skulle hamna utanför spelfältet och returnera false i så fall
         // (jämför med metoden SetShape och hur jag kollar om x och y är för stort eller litet)
     }
 
     public void AddShape(int x, int y, Shape shape)
     {
-        SetShape(x,y,shape,true);
+        SetShape(x, y, shape, true);
     }
 
     public void RemoveShape(int x, int y, Shape shape)
     {
-        SetShape(x, y,shape, false);
+        SetShape(x, y, shape, false);
     }
 
     private void SetShape(int baseX, int baseY, Shape shape, bool value)
@@ -57,6 +88,7 @@ public class PlayingArea : MonoBehaviour
         for (var ty = 0; ty < 4; ty++)
         {
             var y = baseY + ty;
+
             if (y < 0 || y >= height)
             {
                 continue;
@@ -65,6 +97,7 @@ public class PlayingArea : MonoBehaviour
             for (var tx = 0; tx < 4; tx++)
             {
                 var x = baseX + tx;
+
                 if (x < 0 || x >= width)
                 {
                     continue;
